d98f39341e2ecabb56099bd751565953ddc9e82c
Rubén García <rubengs.informatica@gmail.com>
2023-05-05 21:12:36 +0200

    validacion de datos de registro terminada

--- src/javafxmlapplication/controller/RegistroSocioController.java
+++ src/javafxmlapplication/controller/RegistroSocioController.java
@@ -7,6 +7,10 @@ package javafxmlapplication.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static javafx.application.Application.launch;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
@@ -14,6 +18,7 @@ import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -28,9 +33,12 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.Booking;
+import model.Club;
+import model.ClubDAOException;
+import model.Member;
 
-public class RegistroSocioController {
-    
+public class RegistroSocioController implements Initializable{
+
     @FXML
     private Label label_inicioSesion;
     @FXML
@@ -67,75 +75,105 @@ public class RegistroSocioController {
     private Label errorTarjeta;
     @FXML
     private Label errorSVC;
-    
-    @FXML
-    private void registrarse(ActionEvent event) {
+
+    /**
+     * Initializes the controller class.
+     */
+    public void initialize(URL url, ResourceBundle rb) {
         
+        System.out.println("working");
         // Crear binding para el label de error del campo "nombre"
-        BooleanBinding nombreValido = Bindings.createBooleanBinding(() -> {
+        BooleanBinding nombreNoValido = Bindings.createBooleanBinding(() -> {
             String nombre = input_nombre.getText();
             return nombre.isEmpty();
         }, input_nombre.textProperty());
-        
-        errorNombre.visibleProperty().bind(nombreValido);
+
+        errorNombre.visibleProperty().bind(nombreNoValido);
 
         // Crear binding para el label de error del campo "apellidos"
-        BooleanBinding apellidosValidos = Bindings.createBooleanBinding(() -> {
+        BooleanBinding apellidosNoValidos = Bindings.createBooleanBinding(() -> {
             String apellidos = input_apellidos.getText();
             return apellidos.isEmpty();
         }, input_apellidos.textProperty());
-        errorApellidos.visibleProperty().bind(apellidosValidos);
+        errorApellidos.visibleProperty().bind(apellidosNoValidos);
 
         // Crear binding para el label de error del campo "telefono"
-        BooleanBinding telefonoValido = Bindings.createBooleanBinding(() -> {
+        //NOTA: NO SE SI ES NECESARIO HACER EL REGEX - PREGUNTAR AL PROFESOR
+        BooleanBinding telefonoNoValido = Bindings.createBooleanBinding(() -> {
+            String regexTel = "\\d{9,}";
             String telefono = input_telefono.getText();
-            return telefono.isEmpty();
+            return telefono.isEmpty() || !telefono.matches(regexTel);
         }, input_telefono.textProperty());
-        errorTelefono.visibleProperty().bind(telefonoValido);
+        errorTelefono.visibleProperty().bind(telefonoNoValido);
 
         // Crear binding para el label de error del campo "usuario"
-        BooleanBinding usuarioValido = Bindings.createBooleanBinding(() -> {
+        BooleanBinding usuarioNoValido = Bindings.createBooleanBinding(() -> {
             String usuario = input_usuario.getText();
             return usuario.isEmpty();
         }, input_usuario.textProperty());
-        errorUsuario.visibleProperty().bind(usuarioValido);
+        errorUsuario.visibleProperty().bind(usuarioNoValido);
 
         // Crear binding para el label de error del campo "contrasena"
-        BooleanBinding passwordValido = Bindings.createBooleanBinding(() -> input_password.getText().length() < 6, input_password.textProperty());
-        errorPassword.visibleProperty().bind(passwordValido);
+        BooleanBinding passwordNoValido = Bindings.createBooleanBinding(() -> input_password.getText().length() <= 6, input_password.textProperty());
+        errorPassword.visibleProperty().bind(passwordNoValido);
 
         // Crear bindings para el número de tarjeta de crédito y el código de seguridad SVC
         // Crear un binding para el número de tarjeta de crédito
-        BooleanBinding numeroValido = Bindings.createBooleanBinding(() -> {
+        BooleanBinding numeroNoValido = Bindings.createBooleanBinding(() -> {
             // Crear una expresión regular para validar el número de tarjeta de crédito
             String regexNumero = "\\d{16}";
             String numero = input_tarjeta.getText();
             String codigo = input_svc.getText();
             return !(numero.matches(regexNumero) || (numero.isEmpty() && codigo.isEmpty()));
-        }, input_tarjeta.textProperty());
+        }, input_tarjeta.textProperty(), input_svc.textProperty());
 
         // Crear un binding para el código de seguridad SVC
-        BooleanBinding codigoValido = Bindings.createBooleanBinding(() -> {
+        BooleanBinding codigoNoValido = Bindings.createBooleanBinding(() -> {
             // Crear una expresión regular para validar el código de seguridad SVC
             String regexSVC = "\\d{3}";
             String codigo = input_svc.getText();
             String numero = input_tarjeta.getText();
-            return !(numero.matches(regexSVC) || (numero.isEmpty() && codigo.isEmpty()));
-        }, input_svc.textProperty());
-        
-        errorTarjeta.visibleProperty().bind(numeroValido);
-        errorSVC.visibleProperty().bind(codigoValido);
-        
+            return !(codigo.matches(regexSVC) || (numero.isEmpty() && codigo.isEmpty()));
+        }, input_svc.textProperty(), input_tarjeta.textProperty());
+
+        errorTarjeta.visibleProperty().bind(numeroNoValido);
+        errorSVC.visibleProperty().bind(codigoNoValido);
+
         //si todos los campos son validos -> registrarse
         //COMPLETAR
-                
+        // Crear un binding que dependa de todos los bindings anteriores
+        BooleanBinding datosValidos = Bindings.or(nombreNoValido, apellidosNoValidos)
+                .or(telefonoNoValido).or(passwordNoValido).or(usuarioNoValido).or(numeroNoValido).or(codigoNoValido);
+
+        // Vincular el binding resultante con la propiedad visible del label de error global
+        btn_registrarse.disableProperty().bind(datosValidos);
     }
-    
+
+    //DA ERROR NO SE PORQUE - PREGUNTAR AL PROFESOR
+    @FXML
+    private void registrarse(ActionEvent event) {
+        try {
+            Club.getInstance().registerMember(
+                    input_nombre.getText(),
+                    input_apellidos.getText(),
+                    input_telefono.getText(),
+                    input_usuario.getText(),
+                    input_password.getText(),
+                    "",
+                    0,
+                    null);
+        } catch (IOException ex) {
+            Logger.getLogger(RegistroSocioController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClubDAOException ex) {
+            Logger.getLogger(RegistroSocioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     @FXML
     private void cambiarAInicioSesion() throws IOException {
         Stage currentStage = (Stage) label_inicioSesion.getScene().getWindow();
         currentStage.close();
-        
+
         FXMLLoader miCargador = new FXMLLoader(getClass().getResource("../view/inicioSesion.fxml"));
         Parent root = miCargador.load();
         Scene scene = new Scene(root);
@@ -145,22 +183,14 @@ public class RegistroSocioController {
         stage.initModality(Modality.WINDOW_MODAL);
         stage.initOwner(currentStage);
         stage.getIcons().add(new Image("images/greenball.png"));
-        
+
         stage.show();
-        
+
     }
-    
+
     @FXML
     private void cambiarImagen(ActionEvent event) throws IOException {
         //COMPLETAR
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-        
-    }
-    
 }
