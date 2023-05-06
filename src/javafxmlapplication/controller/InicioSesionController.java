119547973ca979d2879ac36b648631f8e252b8af
Rubén García <rubengs.informatica@gmail.com>
2023-05-06 14:24:59 +0200

    inicio de sesion casi terminado

--- src/javafxmlapplication/controller/InicioSesionController.java
+++ src/javafxmlapplication/controller/InicioSesionController.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,12 +18,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javax.naming.Binding;
 import model.Club;
 import model.ClubDAOException;
+import model.Member;
 
 /**
  * FXML Controller class
@@ -35,13 +39,28 @@ public class InicioSesionController implements Initializable {
     private Button btn_registro;
     @FXML
     private Label label_passOlvidada;
+    @FXML
+    private TextField input_usuario;
+    @FXML
+    private TextField input_password;
+    @FXML
+    private Button btn_iniciarSesion;
+    @FXML
+    private Label label_errorPassword;
+    @FXML
+    private Label label_errorUsuario;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //TODO
+        
+        //bindear boton inicio de sesion con los campos
+        btn_iniciarSesion.disableProperty().bind(
+                Bindings.or(Bindings.isEmpty(input_usuario.textProperty()), Bindings.isEmpty(input_password.textProperty()))
+        );
+        
     }
 
     @FXML
@@ -61,6 +80,34 @@ public class InicioSesionController implements Initializable {
 
         stage.show();
 
+    }
+
+    @FXML
+    private void iniciarSesion(ActionEvent event) throws ClubDAOException, IOException {
+        
+        //por defecto
+        label_errorPassword.visibleProperty().setValue(false);
+        label_errorUsuario.visibleProperty().setValue(false);
+        
+        //obtenemos si el usuario existe
+        Boolean exists = Club.getInstance().existsLogin(input_usuario.getText());
+        System.out.println(exists);
+        //si no existe mostramos el error
+        label_errorUsuario.visibleProperty().setValue(!exists);
+        
+        
+        //miramos si la contrasena es correcta
+        if(exists){
+            Member member = Club.getInstance().getMemberByCredentials(input_usuario.getText(), input_password.getText());
+            //si no es correcta mostramos error
+            label_errorPassword.visibleProperty().setValue(member == null);
+            
+            if(member != null){
+                System.out.println("Inicio de sesion correcto");
+                //COMPLETAR - INICIO DE SESION CORRECTO
+            }
+        }
+        
     }
 
 }
