f57094ebb8a07643ddb0ac195ea79192e1cbef81
Rubén García Sánchez <rubengs.informatica@gmail.com>
2023-05-12 10:55:36 +0200

    vistaPrincipalNoRegistrado ahora es "verPistas", ventana de inicio creada

---
+++ src/javafxmlapplication/controller/InicioAppController.java
@@ -0,0 +1,33 @@
+package javafxmlapplication.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ruben
+ */
+public class InicioAppController {
+
+    @FXML
+    private Button btn_verPistas;
+    @FXML
+    private Button btn_IniciarSesion;
+    @FXML
+    private Button btn_registro;
+
+    @FXML
+    private void iniciarSesion(ActionEvent event) {
+    }
+
+    @FXML
+    private void cambiarARegistro(ActionEvent event) {
+    }
+    
+}
