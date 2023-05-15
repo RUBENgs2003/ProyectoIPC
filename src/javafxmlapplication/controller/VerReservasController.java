9f379d29f9640cc93d8cdf4e8778832d708cff07
Gabriel <gsandos@upv.es>
2023-05-15 23:07:07 +0200

    Ver mis reservas

---
+++ src/javafxmlapplication/controller/VerReservasController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import model.Member;
+
+/**
+ * FXML Controller class
+ *
+ * @author X
+ */
+public class VerReservasController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    void setMember(Member member) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
